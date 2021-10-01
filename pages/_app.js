4228b165cfbcff699f2562756a33c4f21e0c63bc
Mihirss <mihirsoniTM@gmail.com>
2021-10-01 10:52:44 +0530

    Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from '../Components/Navbar';
+import Home from '../MenuPages/Home';
+import Services from '../MenuPages/Services';
+import Contact from '../MenuPages/Contact';
+import About from '../MenuPages/About';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function _app() {
+  return (
+    <div>
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route path="/sevices" component={Services} />
+      </Switch>
+    </Router>
+    </div>
+  )
 }
 
-export default MyApp
+export default _app
+
